c5c8b07c0d25905176a726b402ada5b4cda67fbb
cragtok <lidfun@protonmail.com>
2022-12-17 17:53:19 +0600

    Pass props to ProductTable and add filtering logic

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,11 +1,19 @@
 import ProductCategoryRow from "./ProductCategoryRow.js";
 import ProductRow from "./ProductRow.js";
 
-function ProductTable({ products }) {
+function ProductTable({ products, filterText, inStockOnly }) {
     const rows = [];
     let lastCategory = null;
 
     products.forEach((product) => {
+        if (
+            product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1
+        ) {
+            return;
+        }
+        if (inStockOnly && !product.stocked) {
+            return;
+        }
         if (product.category !== lastCategory) {
             rows.push(
                 <ProductCategoryRow
